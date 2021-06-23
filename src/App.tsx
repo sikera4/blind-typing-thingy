fc4bf25dfb86c645f9802ba3af9c787768eb654e
Andrew <sikera0812@mail.ru>
2021-06-23 22:32:07 +0300

    - implement all the needed functionality: - implement typing speed tracker (signs per minute & words per minute) - implement accuracy tracker - implement changer of the resource that the app takes the text from - make header to be in a papyrus font (just for fun)

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,87 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import someTextFetcher from './utils/text';
 import useKeyPress from './hooks/useKeyPress';
+import { currentTime } from './utils/time';
 
 function App() {
-  const [text, setText] = useState('');
+  // states for stats (Sign per minute and words per minute)
+  const [startTime, setStartTime] = useState<number>();
+  const [signCount, setSignCount] = useState<number>(0);
+  const [wordCount, setWordCount] = useState<number>(0);
+  const [spm, setSpm] = useState<string>('0');
+  const [wpm, setWpm] = useState<string>('0');
+  const [accuracy, setAccuracy] = useState<string>('0');
+
+  // states for the typable text
+  const [text, setText] = useState<string>('');
+  const [leftPadding, setLeftPadding] = useState<string>(
+    new Array(20).fill(' ').join(''),
+  );
+  const [outgoingChars, setOutgoingChars] = useState<string>('');
+  const [currentChar, setCurrentChar] = useState<string>('');
+  const [incomingChars, setIncomingChars] = useState<string>('');
+  const [typedChars, setTypedChars] = useState<string>('');
+  
+  // state for wrong char reaction
+  const [wrongChar, setWrongChar] = useState<boolean>(false);
+
+  // states for data fetching
+  const [api, setApi] = useState<string>('https://animechan.vercel.app/api/random');
+  const [fetchCounter, setFetchCounter] = useState<number>(0);
 
   useEffect(() => {
-    fetch('https://animechan.vercel.app/api/random')
+    fetch(api)
     .then((res) => {
       return res.json();
     })
     .then(data => {
-      console.log(data.quote);
-      setText(data.quote);
-      setCurrentChar(data.quote.charAt(0));
-      setIncomingChars(data.quote.substr(1));
-    });
-  }, [])
-
-  const [leftPadding, setLeftPadding] = useState(
-    new Array(20).fill(' ').join(''),
-  );
-
-  const [outgoingChars, setOutgoingChars] = useState('');
-  const [currentChar, setCurrentChar] = useState(text.charAt(0));
-  const [incomingChars, setIncomingChars] = useState(text.substr(1));
+      let neededText = '';
+      if (api === 'https://api.quotable.io/random') {
+        neededText = data.content;
+      } else {
+        neededText = data.quote; 
+      }
+      console.log(neededText);
+      setText(neededText);
+      setCurrentChar(neededText.charAt(0));
+      setIncomingChars(neededText.substr(1));
+    }).catch(err => setText(err));
+  }, [api, fetchCounter])
   
   useKeyPress((key) => {
     //1
-    let updatedOutgoingChars = outgoingChars;
-    let updatedIncomingChars = incomingChars;
+    let updatedOutgoingChars:string = outgoingChars;
+    let updatedIncomingChars:string = incomingChars;
     
+    if (!startTime) {
+      setStartTime(currentTime());
+    }
+
     //2
     if (key === currentChar) {
       //3
+
+      setWrongChar(false);
+      const updatedTypedChars:string = typedChars + key;
+      setTypedChars(updatedTypedChars);
+
+      setAccuracy(
+        ((updatedOutgoingChars.length * 100) / updatedTypedChars.length).toFixed(2)
+        );
+
+      setSignCount(signCount + 1);
+      const durationInMinutes = (currentTime() - startTime!) / 60000.0;
+      setSpm(((signCount + 1) / durationInMinutes).toFixed(2));
+
+      if (incomingChars.charAt(0) === ' ') {
+        //4
+        setWordCount(wordCount + 1);
+        //5
+        const durationInMinutes: number = (currentTime() - startTime!) / 60000.0;
+        //6
+        setWpm(((wordCount + 1) / durationInMinutes).toFixed(2));
+      }
+
       if (leftPadding.length > 0) {
         setLeftPadding(leftPadding.substring(1));
       }
@@ -49,20 +95,45 @@ function App() {
       //6
       updatedIncomingChars = incomingChars.substring(1);
       setIncomingChars(updatedIncomingChars);
+    } else {
+      setWrongChar(true);
     }
   })
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main className="App-body">
+        <h1 className="header">BLIND TYPING THINGY</h1>
+        <h2 className="facey">(͠≖ ͜ʖ͠≖)👌</h2>
+        <p className= "whole-string">This is what you are going to type, enjoy (anime quotes by default xD): '{text}'</p>
         <p className="Character">
           <span className="Character-out">
             {(leftPadding + outgoingChars).slice(-20)}
           </span>
-          <span className="Character-current">{currentChar}</span>
+          <span className={wrongChar ? "Character-current-wrong" : "Character-current"}>{currentChar}</span>
           <span>{incomingChars.substr(0, 20)}</span>
         </p>
-      </header>
+        <h3 className="stats">Signs Per Minute: {spm} | Words Per Minute: {wpm}</h3>
+        <h3 className="stats">Accuracy: {accuracy}</h3>
+        <p>If you're finished please choose what you'd like to type:</p>
+        <p className='commentary'>You can choose what you want to type from a WIDE range of opportunities! What are you interested in?</p>
+        <p className='api-changers'>
+          <span className="api-changer" onClick={() => {
+            setApi('https://api.quotable.io/random');
+            setFetchCounter(fetchCounter + 1);
+            }}>Some deep thoughts...</span>
+          <span className="api-changer" onClick={() => {
+            setApi('https://animechan.vercel.app/api/random');
+            setFetchCounter(fetchCounter + 1);
+            }}>Anime quotes!</span>
+          <span className="api-changer" onClick={() => {
+            setApi('https://api.kanye.rest');
+            setFetchCounter(fetchCounter + 1);
+            }}>Maybe... Kanye West quotes?..</span>
+        </p>
+      </main>
     </div>
   );
 }
--- src/utils/text.ts
+++
@@ -1,9 +0,0 @@
-
-const someTextFetcher = async () => {
-  const response = await fetch('https://animechan.vercel.app/api/random');
-  const data = await response.json();
-  return data;
-}
-
-
-export default someTextFetcher;
